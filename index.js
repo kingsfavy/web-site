1f800f415125279978e3180e1377ce6f41598795
kingsfavy <122705122+kingsfavy@users.noreply.github.com>
2023-03-03 16:32:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+
+function getValue()
+{
+      if{confirm(var n = [];
+      var a = n.slice(0,1);
+      var b = n.slice(1,n.length);
+      var c = a.toUpperCase();
+      var d = b.toLowerCase();
+     var e = c + d }}{
+     alert("delivered");
+     
+     else{
+       {"please input a value");
+     
+    var name =  document.getElementsByName("name")[0].Value;
+   var email =  document.getElementsByName("email")[0].Value;
+ var message = document.getElementsByName("message")[0].Value;
+       }
+     }
+ }
+
+function validateForm() {
+         var firstName = document.getElementById("Name").value;
+         if(Name == "") {
+            alert("Please enter a value on the text field.");
+            return false;
+         }
+      }
+      
+      function validateForm() {
+         var Email = document.getElementById("Email").value;
+         if(Email== "") {
+            alert("Please enter a value on the text field.");
+            return false;
+         }
+      }
+      
+      function validateForm() {
+         var Message = document.getElementById("Message").value;
+         if(Message == "") {
+            alert("Please enter a value on the text field.");
+            return false;
+         }
+      }
+      
+      function SendPostRequest(){
+  alert("message delivered");
+}
+document.getElementsByName("input").classList.add("nav");
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myContainer myAnimation");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+function sendPostRequest(){
+  document.getElementById("mit"). innerHTML ="thank you for your contributions, you will reach out to you through your email.";
+}
